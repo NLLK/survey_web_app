8e110f1b0e9a8e3a7d651980bc760d1fd6c9ed38
nllk <babanov-artem@yandex.ru>
2022-01-31 18:39:46 +0600

    playing with material ui

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import './styles.css'
+import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  height: "300px",
+  paddingBlockEnd: "5px"
+}));
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,10 +27,24 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
         <MisteryBox width="150px" height="150px" text="any other"></MisteryBox>
       </div>
-        
-        
+
+
     );
   }
 }
@@ -27,33 +53,32 @@ class MisteryBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text : this.props.text
+      text: this.props.text
     };
     this.click = this.click.bind(this);
   }
-  click()
-  {
+  click() {
     this.setState((prevState) => {
       return {
-        text: prevState.text === "aboba"? "bobaba": "aboba"
+        text: prevState.text === "aboba" ? "bobaba" : "aboba"
       };
     });
   }
   render() {
-    let style =  {
-      width : this.props.width,
+    let style = {
+      width: this.props.width,
       height: this.props.height
     }
     return (
-        <div className="misteryBox" onClick={this.click} style={style}>
-          <p>{this.state.text}</p>
-        </div>
+      <div className="misteryBox" onClick={this.click} style={style}>
+        <p>{this.state.text}</p>
+      </div>
     );
   }
 }
 
 
-//export default App;
+export default App;
 
-App.defaultProps = {aboba: "abobus", amoga: "amogus"}
+App.defaultProps = { aboba: "abobus", amoga: "amogus" }
 render(<App />, document.getElementById("app"));
