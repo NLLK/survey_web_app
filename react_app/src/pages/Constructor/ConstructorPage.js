ebe7dcf1ff3790c343281c1997cab5009ceae615
nllk <babanov-artem@yandex.ru>
2022-03-05 16:14:18 +0600

    commit

--- react_app/src/pages/Constructor/ConstructorPage.js
+++ react_app/src/pages/Constructor/ConstructorPage.js
@@ -37,14 +37,14 @@ export default function ConstructorPage(props) {
 
   }, [])
 
-  const UnderConstruction = () => {
-    let i = 0;
-    var rows = [];
-    for (i = 0; i < 50; i++) {
-      rows.push(<span key={i} style={{ fontSize: "30px" }}>UNDER CONSTRUCTION</span>)
-    }
-    return rows
-  }
+  // const UnderConstruction = () => {
+  //   let i = 0;
+  //   var rows = [];
+  //   for (i = 0; i < 50; i++) {
+  //     rows.push(<span key={i} style={{ fontSize: "30px" }}>UNDER CONSTRUCTION</span>)
+  //   }
+  //   return rows
+  // }
 
   return (
     <>
