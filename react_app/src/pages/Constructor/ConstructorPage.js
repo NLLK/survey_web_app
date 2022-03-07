7ad4996bb29285be270f8af843141fb39c6fc725
nllk <babanov-artem@yandex.ru>
2022-03-07 18:53:02 +0600

    omw to edit questionnaire

--- react_app/src/pages/Constructor/ConstructorPage.js
+++ react_app/src/pages/Constructor/ConstructorPage.js
@@ -1,42 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import './styles.css'
 import { CONSTRUCTOR_MENU } from "../Common/SideBar/SideBarList";
 import SideBarHandler from "../Common/SideBar/SideBarHandler";
 import UserPermissionsWrapper from "../Common/UserPermissionsWrapper";
 import { GetQuestionnaireById, QuestionnaireTemplate } from "../SelectQuestionnaire/QuestionnaireActions";
-// import ViewerButton from "./RegisterViewer/ViewerButton";
-import TextBox from "./RegisterViewer/TextBox";
 
+import { CONSTRUCTOR_SET_QUESTIONNAIRE } from "./Reducer/ConstructorReducerTypes";
+
+import TextBox from "./RegisterViewer/TextBox";
 import RegisterViewer from './RegisterViewer/RegisterViewer'
 import DivWithCoords from "./RegisterViewer/DivWithCoords";
 import ViewerButton from "./RegisterViewer/ViewerButton";
+import { Question, Questionnaire } from "./Models/Models";
 
 export default function ConstructorPage(props) {
 	let params = useParams();
 
 	const navigate = useNavigate()
+	const dispatch = useDispatch()
 
-	//const [questionnaireEditingId, setQuestionnaireEditingId] = useState(-1)
 	const [questionnaireInfo, setQuestionnaireInfo] = useState(QuestionnaireTemplate)
+	const questionnaire = useSelector(state => state.constructor.questionnaire)
+
+
+	const getQuestionnaire = async (id) => {
+
+		const qInfo = Questionnaire.test()//await GetQuestionnaireById(setQuestionnaireInfo, id)
+		console.log('or',await GetQuestionnaireById(setQuestionnaireInfo, id))
+		console.log('test',qInfo)
+
+		let qTest = {
+			...qInfo,
+			fields: JSON.stringify(qInfo.fields)	
+		}
+
+		dispatch({type: CONSTRUCTOR_SET_QUESTIONNAIRE, payload: qTest})
+	}
 
 	useEffect(() => {
 		let id = -1;
 		if (params.id === undefined && props.id === undefined)
 			navigate('/')//TODO: add error page
 		else if (params.id !== undefined) {
-			//setQuestionnaireEditingId(params.id)
 			id = params.id
 		}
 		else {
-			//setQuestionnaireEditingId(props.id)
 			id = props.id
 		}
 
-		if (id > -1) GetQuestionnaireById(setQuestionnaireInfo, id)
-
-
+		if (id > -1) {
+			getQuestionnaire(id)
+		}
 		// let q = JSON.stringify(Question.test())
 		// console.log(q)
 		// setQuestionnaireInfo(prevState => ({
@@ -44,91 +61,37 @@ export default function ConstructorPage(props) {
 		//   fields: q,
 		// }));
 
-
-
 	}, [navigate, params, props])
-
-
-	// const UnderConstruction = () => {
-	//   let i = 0;
-	//   var rows = [];
-	//   for (i = 0; i < 50; i++) {
-	//     rows.push(<span key={i} style={{ fontSize: "30px" }}>UNDER CONSTRUCTION</span>)
-	//   }
-	//   return rows
-	// }
 
 	return (
 		<>
 			{/* <UserPermissionsWrapper permission={2} /> */}
-			<SideBarHandler page_name={"Конструктор анкет: " + questionnaireInfo.name} width={300} menu_type={CONSTRUCTOR_MENU} />
-			<div className="constructor">
-				<div className="c-left-part">
-					<div className="c-registerViewer" style={{ overflow: "auto", position: "relative", paddingLeft: "10 px" }}>
-						{
-							RegisterViewer(questionnaireInfo.fields)
-						}
-						{/* <div>
-							<div style={{ position: "relative" }}>
-								<ViewerButton>
-									1
-								</ViewerButton>
-								<div style={{ position: "relative", left: 100 + "px" }}>
-									<ViewerButton>
-										1.1
-									</ViewerButton>
-									<ViewerButton>
-										1.2
-									</ViewerButton>
-									<div style={{ position: "relative" }}>
-										<ViewerButton>
-											1.3.1
-										</ViewerButton>
-										<div style={{ position: "relative", left: 100 + "px" }}>
-											<ViewerButton>
-												1.3.1.1
-											</ViewerButton>
-											<ViewerButton>
-												1.3.1.2
-											</ViewerButton>
-										</div>
-									</div>
-									<ViewerButton>
-										1.4
-									</ViewerButton>
-								</div>
+			{
+				questionnaire ? <>
+					<SideBarHandler page_name={"Конструктор анкет: " + questionnaire.name} width={300} menu_type={CONSTRUCTOR_MENU} />
+					<div className="constructor">
+						<div className="c-left-part">
+							<div className="c-registerViewer" style={{ overflow: "auto", position: "relative", paddingLeft: "10 px" }}>
+								{
+									RegisterViewer(questionnaire.fields)
+								}
+
+								<ViewerButton type="add">+</ViewerButton>
+
 							</div>
+
+							<div className="c-answerEditor">
+
+							</div>
+						</div>w
+						<div className="c-registerEditor">
+
 						</div>
-
-						<div>
-							<div style={{ position: "relative" }}>
-								<ViewerButton>
-									2
-								</ViewerButton>
-								<div style={{ position: "relative", left: 100 + "px" }}>
-									<ViewerButton>
-										2.1
-									</ViewerButton>
-									<ViewerButton>
-										2.2
-									</ViewerButton>
-								</div>
-							</div>
-						</div> */}
-
-
 					</div>
+				</>
+				: <></>
+			}
 
-
-
-					<div className="c-registerEditor">
-
-					</div>
-				</div>
-				<div className="c-registerPicker">
-
-				</div>
-			</div>
 		</>
 
 	);
